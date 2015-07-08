9110c7993666ab074e0b35fada07de4ac3758b6b
Justin Miron <justinmiron11@gmail.com>
2015-07-07 20:39:43 -0700

    added cluster to nodejs server

--- server/server.js
+++ server/server.js
@@ -1,50 +1,59 @@
-var sys = require('sys');
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var mongoHandler = require('mongoHandler');
+var sys = require('sys')
+var http = require('http')
+var url = require('url')
+var fs = require('fs')
+var mongoHandler = require('mongoHandler')
+
 
 var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+    callback(req,res)
 });
 
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(server)
 
 io.sockets.on('connection', function (socket) {
-    console.log('A user has connected to the server');
-    socket.broadcast.emit('new_user');
+    console.log('A user has connected to the server')
+    socket.broadcast.emit('new_user')
 
     /* Chat related listeners */
     //TODO: Add username related information
     socket.on('chat_user_join', function(){
-    	socket.broadcast.emit('user_join_chat');
-    	console.log('A user has joined a chat room');
+    	socket.broadcast.emit('user_join_chat')
+    	console.log('A user has joined a chat room')
     })
 
     socket.on('chat_user_left', function(){
-        socket.broadcast.emit('user_left');
-        console.log('User has left the chat room');
-    });
+        socket.broadcast.emit('user_left')
+        console.log('User has left the chat room')
+    })
 
     socket.on('chat_new_message', function (message) {
-        console.log('New message posted in the chat: ' + message);
-        socket.broadcast.emit('new_message', message);
-    });
-
+        console.log('New message posted in the chat: ' + message)
+        socket.broadcast.emit('new_message', message)
+    })
 
     /* Match related listeners */
     socket.on('new_profile', function(profile){
-    	console.log("Received profile");
-    	var username = profile.username;
-    	var band1 = profile.band1;
-    	var band2 = profile.band2;
-    	var band3 = profile.band3;
+    	console.log("Received profile")
+    	var username = profile.username
+    	var band1 = profile.band1
+    	var band2 = profile.band2
+    	var band3 = profile.band3
 
-    	mongoHandler.modifyProfile(username, band1, band2, band3);
-    });
-});
-server.listen(8080);
+    	mongoHandler.modifyProfile(username, band1, band2, band3)
+    })
+
+    socket.on('find_match', function(profile){
+        var username = profile.username
+        var band1 = profile.band1
+        var band2 = profile.band2
+        var band3 = profile.band3
+        mongoHandler.find_match(username, socket.id, band1, band2, band3)
+    })
+
+    socket.on('disconnect', function(){
+
+    })
+})
+server.listen(8080)
 console.log("Server Running on 8080");
